16a91ca890afd062fb298b0ed04ddd2544c731c0
jeanmello33 <jean.mello9@gmail.com>
2024-02-24 15:56:35 -0300

    Blog de Exemplo para Dominar do Angular

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string = "https://pbs.twimg.com/profile_images/1093320615430213632/dA2G7rl7_400x400.jpg"
+
+  @Input()
+  cardTitle:string = "Games dos Principais Personagens da Marvel"
 
   constructor() { }
 
